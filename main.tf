51e79bb46e614cf78f7d639dfaedd2b399502ddd
Nam-ks <147022481+Nam-ks@users.noreply.github.com>
2024-07-24 16:11:11 +0900

    first comit

---
+++ main.tf
@@ -0,0 +1,29 @@
+terraform {
+  cloud {
+    organization = "tf-cn"
+
+    workspaces {
+      name = "nam-aft-test"
+    }
+  }
+}
+
+
+module "aftnamtest" {
+  source  = "app.terraform.io/tf-cn/aftnamtest/github"
+  version = "1.1.0"
+  # insert required variables here
+    ct_management_account_id    = "552166050235"
+    log_archive_account_id      = "590184073128"
+    audit_account_id            = "767397899115"
+    aft_management_account_id   = "891377167096"
+    github_username             = "Nam-ks"
+    ct_home_region              = "ap-northeast-2"
+    tf_backend_secondary_region = "ap-northeast-1"
+
+    vcs_provider                                  = "github"
+    account_request_repo_name                     = "${var.github_username}/learn-terraform-aft-account-request"
+    account_provisioning_customizations_repo_name = "${var.github_username}/learn-terraform-aft-account-provisioning-customizations"
+    global_customizations_repo_name               = "${var.github_username}/learn-terraform-aft-global-customizations"
+    account_customizations_repo_name              = "${var.github_username}/learn-terraform-aft-account-customizations" 
+}
